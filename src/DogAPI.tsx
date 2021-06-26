c7a7fd6d8fa1ff2df8120a8b8397ea7c5a91738e
A_ito <BoxPistols@users.noreply.github.com>
2021-06-26 22:16:55 +0900

    clear code

--- src/DogAPI.tsx
+++ src/DogAPI.tsx
@@ -3,12 +3,24 @@ import axios from 'axios'
 
 export default function DogApi() {
   const onClickUsers = () => {
-    axios.get('https://jsonplaceholder.typicode.com/users').then((res) => {
-      console.log(res.data[0].name)
-    })
+    axios
+      .get('https://jsonplaceholder.typicode.com/users')
+      .then((res) => {
+        console.log(res.data[0].name)
+      })
+      .catch((err) => {
+        console.log('oh! : ' + err)
+      })
   }
   const onClickUsers1 = () => {
-    alert('a')
+    axios
+      .get('https://jsonplaceholder.typicode.com/users?id=1')
+      .then((res) => {
+        console.log(res.data[0].email)
+      })
+      .catch((err) => {
+        console.log('oh! : ' + err)
+      })
   }
   return (
     <div>
